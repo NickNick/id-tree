9dedb30895745be76c1d765e99b3c0c3a7dca7b4
Nick Overdijk <nick@astrant.net>
2022-04-14 19:50:06 +0200

    Clippy denies deriving Hash but not PartialEq. Remove the unused Hash.

--- src/node.rs
+++ src/node.rs
@@ -75,7 +75,7 @@ impl<T> NodeBuilder<T> {
 ///
 /// A container that wraps data in a given `Tree`.
 ///
-#[derive(Debug, Clone, Hash, Eq)]
+#[derive(Debug, Clone, Eq)]
 #[cfg_attr(feature = "serde_support", derive(Serialize, Deserialize))]
 pub struct Node<T> {
     pub(crate) data: T,
